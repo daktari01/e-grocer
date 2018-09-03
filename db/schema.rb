f534f48ca7483a581686563714b4d5a0e1424623
James Dindi <dindijjames@gmail.com>
2018-09-04 01:13:11 +0300

    Add helpers for stock

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_080612) do
+ActiveRecord::Schema.define(version: 2018_09_03_215728) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_080612) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stock", default: 0
   end
 
 end
