d46cbc53fa55092d5a4b8e2fdd99bc65eb50baa7
James Dindi <dindijjames@gmail.com>
2018-09-04 22:03:14 +0300

    Add stock

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
-    validates_numericality_of :price
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    validates :stock, numericality: { greater_than_or_equal_to: 0,  only_integer: true }
 end
