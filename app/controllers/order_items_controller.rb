0c905b9afa85bdc1a954fcb1e5c1691746fe0e24
James Dindi <dindijjames@gmail.com>
2018-09-05 10:34:13 +0300

    Add product to cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,5 @@
 class OrderItemsController < ApplicationController
+  before_action :load_order, only:[:create]
   before_action :set_order_item, only: [:show, :edit, :update, :destroy]
 
   # GET /order_items
@@ -24,11 +25,12 @@ class OrderItemsController < ApplicationController
   # POST /order_items
   # POST /order_items.json
   def create
-    @order_item = OrderItem.new(order_item_params)
+    @order_item = OrderItem.new(product_id: params[:product_id], order_id: @order.id)
 
     respond_to do |format|
       if @order_item.save
-        format.html { redirect_to @order_item, notice: 'Order item was successfully created.' }
+        # format.html { redirect_to @order_item, notice: 'Order item was successfully created.' }
+        format.html { redirect_to @order, notice: 'Successfully added product to cart.' }
         format.json { render :show, status: :created, location: @order_item }
       else
         format.html { render :new }
@@ -62,6 +64,15 @@ class OrderItemsController < ApplicationController
   end
 
   private
+    def load_order
+      begin
+        @order = Order.find(session[:order_id])
+      rescue ActiveRecord::RecordNotFound
+        @order = Order.create(status: "unsubmitted")
+        session[:order_id] = @order.id
+      end
+    end
+    
     # Use callbacks to share common setup or constraints between actions.
     def set_order_item
       @order_item = OrderItem.find(params[:id])
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 0 }
     validates :stock, numericality: { greater_than_or_equal_to: 0,  only_integer: true }
+    has_many :order_items
 end
